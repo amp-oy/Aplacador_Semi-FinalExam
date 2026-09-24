149a781301f2a32891528c49f9697bd4798d39fa
amp-oy <aplacadorjp12@gmail.com>
2026-09-24 10:21:52 +0800

    Add tasks composable

--- src/composables/useTasks.js
+++ src/composables/useTasks.js
@@ -1,10 +1,11 @@
-import { ref } from 'vue'
+import {ref, computed, watch, onMounted} from 'vue'
 
-const STORAGE_KEY = 'tasks'
+const STORAGE_KEY = 'student-task-manager-tasks'
 
 export function useTasks() {
   const tasks = ref([])
 
+  
   function loadTasks() {
     try {
       const savedTasks = localStorage.getItem(STORAGE_KEY)
@@ -18,18 +19,68 @@ export function useTasks() {
     }
   }
 
+  
   function addTask(task) {
     tasks.value.push(task)
   }
 
+  
   function removeTask(id) {
-    tasks.value = tasks.value.filter(task => task.id !== id)
+    tasks.value = tasks.value.filter(
+      task => task.id !== id
+    )
   }
+
+  
+  function toggleTaskStatus(id) {
+    const task = tasks.value.find(
+      task => task.id === id
+    )
+
+    if (task) {
+      task.completed = !task.completed
+    }
+  }
+
+  
+  const completedTasks = computed(() => {
+    return tasks.value.filter(
+      task => task.completed
+    ).length
+  })
+
+  
+  const pendingTasks = computed(() => {
+    return tasks.value.filter(
+      task => !task.completed
+    ).length
+  })
+
+  
+  watch(
+    tasks,
+    (newTasks) => {
+      localStorage.setItem(
+        STORAGE_KEY,
+        JSON.stringify(newTasks)
+      )
+    },
+    {
+      deep: true
+    }
+  )
+
+  
+  onMounted(() => {
+    loadTasks()
+  })
 
   return {
     tasks,
-    loadTasks,
+    completedTasks,
+    pendingTasks,
     addTask,
-    removeTask
+    removeTask,
+    toggleTaskStatus
   }
 }
